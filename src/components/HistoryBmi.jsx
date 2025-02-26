101db232a124a2f20296db4c043ee97892853035
hudzzz01 <hdzzzzzz01@gmail.com>
2025-02-26 16:33:19 +0700

    feat : page component history bmi

--- src/components/HistoryBmi.jsx
+++ src/components/HistoryBmi.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const HistoryBmi = () => {
     return (
-        <div>
-         
+        <div classNameName=''>
+            <div className='d-flex justify-content-center mt-5 bg-dark-green'>
+
+                <div className="card mb-3 bg-semi-green" style={{ width: "50rem" }}>
+                    <div className="card-header">
+                        History
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item bg-light-green">An item</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
